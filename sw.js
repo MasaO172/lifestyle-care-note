274cda74c1d504a2e6118edfc7a070618ea0ef02
MasaO172 <600137om@sakura.med.toho-u.ac.jp>
2026-09-13 16:57:51 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX="lifestyle-care-note-";
-const CACHE=CACHE_PREFIX+"v47";
+const CACHE=CACHE_PREFIX+"v48";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./manual_device_01.png","./manual_device_02.png","./manual_device_03.png","./manual_device_04.png","./manual_device_05.png","./manual_device_06.png","./manual_app_01.png","./manual_app_02.png","./manual_app_03.png","./manual_app_04.png","./manual_app_05.png","./manual_app_06.png","./manual_app_07.png","./manual_app_08.png"];
 
 self.addEventListener("install",event=>{
